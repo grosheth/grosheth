0e8532ff80a9218610e248da304ee31dc3d902e4
salledelavage <salledelavager@gmail.com>
2024-08-22 07:38:13 -0400

    add functions

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import requests
 import dotenv, os, json
 
-dotenv.load_dotenv()
-username = os.getenv("USERNAME")
-token = os.getenv("TOKEN")
 
-login = requests.get('https://api.github.com/search/repositories?q=github+api', auth=(username,token))
+LANGS = {"Python": 0,"Nix": 0,"HTML": 0, "Go": 0, "Lua": 0, "JavaScript": 0, "Shell": 0, "Dockerfile": 0}
 
-repositories = requests.get('https://api.github.com/users/grosheth/repos', auth=(username,token))
+def get_credentials():
+    dotenv.load_dotenv()
+    username = os.getenv("USERNAME")
+    token = os.getenv("TOKEN")
+    return username, token
 
-for repo in json.loads(repositories.text):
-    language = requests.get(f"https://api.github.com/repos/grosheth/{repo['name']}/languages", auth=(username,token))
-    print(json.loads(language.text))
+def login():
+    username, token = get_credentials()
+    login = requests.get('https://api.github.com/search/repositories?q=github+api', auth=(username,token))
+    return login
 
+
+def get_langs():
+
+    username, token = get_credentials()
+    repositories = requests.get('https://api.github.com/users/grosheth/repos', auth=(username,token))
+
+    for repo in json.loads(repositories.text):
+        language = requests.get(f"https://api.github.com/repos/grosheth/{repo['name']}/languages", auth=(username,token))
+        print(json.loads(language.text))
+
+
+get_langs()
+
+
+
+    
     # build a dictionnary with values and sum values
     # Then build graph
+
+
+
+
+
+# Make a graph of percentage per repo
